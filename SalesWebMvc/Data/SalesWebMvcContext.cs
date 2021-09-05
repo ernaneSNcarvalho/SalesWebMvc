8e33391eef309307c29f5a637064d033438bb82c
ernane <ernanecarvalho89@gmail.com>
2021-09-05 20:58:05 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -10,5 +10,8 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<SalesWebMvc.Models.Departament> Departament { get; set; }
+        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecord { get; set; }
+        public DbSet<SalesWebMvc.Models.Seller> Sellers { get; set; }
+
     }
 }
