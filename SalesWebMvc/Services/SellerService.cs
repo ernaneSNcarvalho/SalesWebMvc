0a197afcfcd2fed9ff6a950451ae5f40c8448801
ernane <ernanecarvalho89@gmail.com>
2021-09-07 17:20:31 -0300

    Tela simples cadastro COM ERRO

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,5 +17,10 @@ namespace SalesWebMvc.services
         {
             return _context.Sellers.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
